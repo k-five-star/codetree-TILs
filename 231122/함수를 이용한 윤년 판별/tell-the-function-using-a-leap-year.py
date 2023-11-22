af3739d493e5bc52449487343ad3e5f937388f0b
k-five-star <113240065+k-five-star@users.noreply.github.com>
2023-11-22 16:03:51 +0900

    Title: 함수를 이용한 윤년 판별, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231122/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 231122/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -1,7 +1,7 @@
 def tell_leap_year(n):
     if n % 4 == 0:
         if n % 100 == 0:
-            if n % 400:
+            if n % 400 == 0:
                 return True
             
             return False
