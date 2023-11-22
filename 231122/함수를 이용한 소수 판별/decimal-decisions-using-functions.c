ef12209bec19fb66a5738040f46d1d3df988d69e
k-five-star <113240065+k-five-star@users.noreply.github.com>
2023-11-22 15:48:49 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231122/함수를 이용한 소수 판별/decimal-decisions-using-functions.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int summ(int a, int b);
+int isPrime(int n);
+int main() {
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    printf("%d", summ(a, b));
+    return 0;
+}
+
+int summ(int a, int b) {
+    int i, sum = 0;
+
+    for(i = a; i <= b; i++) {
+        if(isPrime(i)) {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
+
+int isPrime(int n) {
+    int i;
+
+    for(i = 2; i <= n / 2; i++) {
+        if(n % i == 0) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
