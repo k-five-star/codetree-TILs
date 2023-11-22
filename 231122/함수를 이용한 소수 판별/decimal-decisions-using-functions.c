7b9119f64d8ad2f09dab02430752ffd879cb0983
k-five-star <113240065+k-five-star@users.noreply.github.com>
2023-11-22 15:51:07 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231122/함수를 이용한 소수 판별/decimal-decisions-using-functions.c
+++ 231122/함수를 이용한 소수 판별/decimal-decisions-using-functions.c
@@ -25,6 +25,9 @@ int summ(int a, int b) {
 int isPrime(int n) {
     int i;
 
+    if(n == 1)
+        return 0;
+
     for(i = 2; i <= n / 2; i++) {
         if(n % i == 0) {
             return 0;
