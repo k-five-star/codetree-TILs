d36fd31666b40c04c78787b5f6687f145eb3a3fc
k-five-star <113240065+k-five-star@users.noreply.github.com>
2023-11-25 02:01:28 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231125/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -0,0 +1,23 @@
+def check(seq1, seq2):
+    p1 = p2 = 0
+
+    while p1 < len(seq1):
+        if seq1[p1] == seq2[p2]:
+            p1+=1
+            p2+=1
+
+        else:
+            p1+=1
+            p2 = 0
+
+        if p2 == len(seq2):
+            return True
+
+    return False
+
+
+tuple(map(int, input().split()))
+seq1 = list(map(int, input().split()))
+seq2 = list(map(int, input().split()))
+
+print("Yes" if check(seq1, seq2) else "False")
