89b677a8314230d1749e3a58a60003e60ee2df34
k-five-star <113240065+k-five-star@users.noreply.github.com>
2023-12-04 20:35:34 +0900

    Title: 연속된 숫자 만들기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231204/연속된 숫자 만들기/create-consecutive-numbers.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+int main() {
+    int small, mid, big;
+    int temp;
+    int t1, t2;
+    int cnt = 0;
+    int ts, tm, tb, avg;
+
+
+
+    scanf("%d %d %d", &small, &mid, &big);
+    ts = small;
+    tm = mid;
+    tb = big;
+
+    while(mid - small > 1 || big - mid > 1) {
+        cnt ++;
+
+        t1 = mid - small;
+        t2 = big - mid;
+        
+
+        if(t1 >= t2) { //small과 mid 틈이 더 크니, big과 mid사이에 small이 들어간다. 새로운 것은 그러면 mid 중간 big
+            avg = big + mid;
+            small = mid;
+            mid = avg / 2;
+        }
+
+        else { // 그 반대. 새로운 것은 small 중간 mid
+            avg = small + mid;
+            big = mid;
+            mid = avg / 2;
+        }
+    }
+    
+    printf("%d\n", cnt);
+    cnt = 0;
+    big = tb;
+    small = ts;
+    mid = tm;
+
+    while(mid - small > 1 || big - mid > 1) {
+        cnt++;
+
+        t1 = mid - small;
+        t2 = big - mid;
+
+        if(t1 >= t2) { //small과 mid 틈이 더 크니, 과 mid를 교체
+            avg = small + mid;
+            big = mid;
+            mid = avg / 2;
+        }
+
+        else {
+            avg = big + mid;
+            small = mid;
+            mid = avg / 2;
+        }
+
+
+    }
+
+    printf("%d", cnt);
+    cnt = 0;
+    
+
+    return 0;
+}
