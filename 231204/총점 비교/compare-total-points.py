6664dcc36933a8216bc150d23bb7ecab0dc3275e
k-five-star <113240065+k-five-star@users.noreply.github.com>
2023-12-04 16:32:31 +0900

    Title: 총점 비교, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231204/총점 비교/compare-total-points.py
@@ -0,0 +1,20 @@
+class Student:
+    def __init__(self, name, s1, s2, s3):
+        self.name = name
+        self.s1 = int(s1)
+        self.s2 = int(s2)
+        self.s3 = int(s3)
+
+    def getSum(self):
+        return self.s1 + self.s2, self.s3
+
+    def print_student(self):
+        print(f"{self.name} {self.s1} {self.s2} {self.s3}")
+
+n = int(input())
+
+student_list = [Student(*input().split()) for _ in range(n)]
+student_list.sort(key = lambda x : x.getSum())
+
+for student in student_list:
+    student.print_student()
