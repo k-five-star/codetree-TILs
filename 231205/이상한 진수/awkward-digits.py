eb44bee537a77f4b3d3abb1a83350f567af8283d
k-five-star <113240065+k-five-star@users.noreply.github.com>
2023-12-05 12:19:18 +0900

    Title: 이상한 진수, Time: 5040ms, Memory: 26MB, Status: Time Limit Exceed - CodeTree

--- 231205/이상한 진수/awkward-digits.py
+++ 231205/이상한 진수/awkward-digits.py
@@ -15,12 +15,37 @@ def diff(a, b):
             cnt+=1
     return cnt
 
+
+def find_min_max(a, b):
+    candi_list = []
+
+    idx_a_max = a.find('0')
+    max_a = a[:idx_a_max] + "1" + a[idx_a_max + 1:]
+    candi_list.append(int(max_a, 2))
+
+    idx_b_max = b.find('1') if b.find('1') < b.find('0') else b.find('0')
+    max_b = b[:idx_b_max] + "2" + b[idx_b_max + 1:]
+    candi_list.append(int(max_b, 3))
+
+    idx_a_min = a[1:].find('1') + 1
+    min_a = a[:idx_a_min] + "0" + a[idx_a_min + 1:]
+    candi_list.append(int(min_a, 2))
+
+    idx_b_min = a[1:].find('1') if a[1:].find('1') < a[1:].find('2') else a[1:].find('2')
+    min_b = b[:idx_b_min + 1] + "0" + b[idx_b_min + 2:]
+    candi_list.append(int(min_b, 3))
+
+    return min(candi_list), max(candi_list)
+
+
 SIZE = 1000000000 #범위 : 0~10억
 
 a = input() #str타입
 b = input() #str타입
 
-for n in range(0, SIZE + 1):
+_min, _max = find_min_max(a, b)
+
+for n in range(_min, _max + 1):
     ta = bin(n)
     ta = "0" * (len(a) - len(ta[2:])) + ta[2:]
     tb = trit(n)
