b90a57d980fb665fd6bcc3036b3004eaf91ae26c
k-five-star <113240065+k-five-star@users.noreply.github.com>
2024-02-14 02:52:09 -0800

    Title: 요일 맞추기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240214/요일 맞추기/guess-day-of-week.py
@@ -0,0 +1,18 @@
+dayOfWeek = {
+    0:"Mon",
+    1:"Tue",
+    2:"Wed",
+    3:"Thu",
+    4:"Fri",
+    5:"Sat",
+    6:"Sun"
+}
+
+lastDayOfMonth = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+m1, d1, m2, d2 = map(int, input().split())
+delta = 0
+
+delta += (d2 - d1)
+delta += sum(lastDayOfMonth[m1:m2])
+day = dayOfWeek.get(delta % 7)
+print(day)
