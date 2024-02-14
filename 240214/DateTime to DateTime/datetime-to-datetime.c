ec505f80c56b72106837880325b9eb46f51b33da
k-five-star <113240065+k-five-star@users.noreply.github.com>
2024-02-14 02:29:55 -0800

    Title: DateTime to DateTime, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240214/DateTime to DateTime/datetime-to-datetime.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define MINPERDAY 60*24
+#define MINPERHOUR 60
+#define MINPERMIN 1
+int main() {
+    int a, b, c;
+    int delta_day, delta_hour, delta_min;
+    int elapsed_min = 0;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    delta_day = a - 11;
+    delta_hour = b - 11;
+    delta_min = c - 11;
+
+    elapsed_min = delta_day * MINPERDAY + delta_hour * MINPERHOUR + delta_min * MINPERMIN;
+
+    printf("%d", elapsed_min);
+
+    return 0;
+}
