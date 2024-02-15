74b4ee7c95cc46d72bb15563bac17af113055326
k-five-star <113240065+k-five-star@users.noreply.github.com>
2024-02-15 04:20:53 -0800

    Title: 2진수로 변환하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240215/2진수로 변환하기/convert-to-binary.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+char itoa(int n);
+int main() {
+    //10진수를 2로 나눈 나머지를 적는다.
+        //문자열의 인덱스부분에 적는다.
+        //인덱스를 뒤로 한칸 물린다.
+    //10진수를 2로 나눈다.
+    //그걸 10진수가 0이 될때까지 반복한다.
+
+    //문자열을 인덱스-1에서 0까지 읽는다.
+
+    int dec;
+    int idx = 0;
+    char bin[30];
+
+    scanf("%d", &dec);
+
+    while(dec > 0) {
+        bin[idx] = itoa(dec % 2);
+        idx++;
+
+        dec /= 2;
+    }
+
+    for(int i = idx - 1; i >= 0; i--) 
+        printf("%c", bin[i]);
+
+
+    return 0;
+}
+
+char itoa(int n) {
+    if(n == 1)
+        return '1';
+
+    else if(n == 0) 
+        return '0';
+}
