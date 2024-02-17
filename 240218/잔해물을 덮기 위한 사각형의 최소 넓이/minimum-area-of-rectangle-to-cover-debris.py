eb8eeb527e928f383d6a09ffb17ce0589a55d03d
k-five-star <113240065+k-five-star@users.noreply.github.com>
2024-02-17 15:38:31 -0800

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 150ms, Memory: 54MB, Status: Wrong Answer - CodeTree

--- 240218/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240218/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -1,39 +1,49 @@
-def find_remaining_area(x1, y1, x2, y2, x3, y3, x4, y4):
-    # 겹치는 영역의 좌표를 찾습니다.
-    overlap_x1 = max(x1, x3)
-    overlap_y1 = max(y1, y3)
-    overlap_x2 = min(x2, x4)
-    overlap_y2 = min(y2, y4)
-    
-    # 겹치는 영역의 넓이를 계산합니다.
-    overlap_area = 0
-    if overlap_x1 < overlap_x2 and overlap_y1 < overlap_y2:
-        overlap_area = (overlap_x2 - overlap_x1) * (overlap_y2 - overlap_y1)
+class paper:
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1 + 1000
+        self.y1 = y1 + 1000
+        self.x2 = x2 + 1000
+        self.y2 = y2 + 1000
 
-    # 첫 번째 직사각형의 전체 넓이에서 겹치는 영역의 넓이를 뺍니다.
-    total_area = (x2 - x1) * (y2 - y1)
-    
-    # 겹치는 영역이 첫 번째 직사각형 내에 완전히 포함되지 않는 경우, 남는 영역을 재계산합니다.
-    remaining_area = total_area - overlap_area
-    if overlap_x1 == x1 and overlap_x2 == x2:
-        if overlap_y1 > y1 or overlap_y2 < y2:
-            remaining_area = (x2 - x1) * (y2 - y1) - overlap_area
-    elif overlap_y1 == y1 and overlap_y2 == y2:
-        if overlap_x1 > x1 or overlap_x2 < x2:
-            remaining_area = (x2 - x1) * (y2 - y1) - overlap_area
-    elif overlap_area == 0:  # 겹치는 영역이 없는 경우
-        remaining_area = total_area
+    def fill(self, arr):
+        for i in range(self.x1, self.x2):
+            for j in range(self.y1, self.y2):
+                arr[i][j] = 1
 
-    return remaining_area
+    def erase(self, arr):
+        for i in range(self.x1, self.x2):
+            for j in range(self.y1, self.y2):
+                arr[i][j] = 0
 
-# 입력 받기
-x1, y1, x2, y2 = map(int, input().split())
-x3, y3, x4, y4 = map(int, input().split())
+    def how_large_to_cover(self, arr):
+        cx1, cy1, cx2, cy2 = self.x1, self.y1, self.x2, self.y2
+        flag = 0
 
-# 겹치는 영역의 넓이 계산
-overlap_area = find_overlap_area(x1, y1, x2, y2, x3, y3, x4, y4)
+        for i in range(self.x1, self.x2):
+            for j in range(self.y1, self.y2):
+                if arr[i][j] == 1 and flag == 0:
+                    cx1, cy1 = i, j
+                    flag = 1
 
-# 잔해물의 넓이 계산
-remaining_area = calculate_remaining_area(x1, y1, x2, y2, overlap_area)
+        flag = 0
 
-print(remaining_area)
+        for i in range(self.x2 - 1, self.x1 - 1, -1):
+            for j in range(self.y2 - 1, self.y1 - 1, -1):
+                if arr[i][j] == 1 and flag == 0:
+                    cx2, cy2 = i, j
+                    flag = 1
+
+        large = (cx2 - cx1 + 1) * (cy2 - cy1 + 1)
+        return large
+
+
+arr = [[0 for _ in range(2000)] for _ in range(2000)]
+a, b, c, d = map(int, input().split())
+A = paper(a, b, c, d)
+A.fill(arr)
+
+a, b, c, d = map(int, input().split())
+B = paper(a, b, c, d)
+B.erase(arr)
+
+print(A.how_large_to_cover(arr))
