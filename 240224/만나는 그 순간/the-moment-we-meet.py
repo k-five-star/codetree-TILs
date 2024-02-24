7046749a218b05b25f26135ef853037dfb99bcf6
k-five-star <113240065+k-five-star@users.noreply.github.com>
2024-02-23 23:28:10 -0800

    Title: 만나는 그 순간, Time: 174ms, Memory: 44MB, Status: Passed - CodeTree

--- 240224/만나는 그 순간/the-moment-we-meet.py
+++ 240224/만나는 그 순간/the-moment-we-meet.py
@@ -1,43 +1,23 @@
-# N과 M을 입력받는다.
 N, M = map(int, input().split())
-# A와 B의 이동을 입력받는다.
-A = [tuple(input().split()) for _ in range(N)]
-B = [tuple(input().split()) for _ in range(M)]
+pos_a = [0]
+pos_b = [0]
 
-A = [int(cmd[1]) if cmd[0] == 'R' else int(cmd[1]) * -1 for cmd in A]
-B = [int(cmd[1]) if cmd[0] == 'R' else int(cmd[1]) * -1 for cmd in B]
-# A와 B의 초당 위치를 저장할 배열을 둔다.
-A_table = [0]
-B_table = [0]
+def set_pos(arr, n):
+    for _ in range(n):
+        d, t = input().split()
 
-for time in A:
-    x = time
-    while x != 0:
-        if x > 0:
-            A_table.append(A_table[-1] + 1)
-            x -= 1
-        else : 
-            A_table.append(A_table[-1] - 1)
-            x += 1
+        for _ in range(int(t)):
+            next_pos = arr[-1] + (1 if d == 'R' else -1)
+            arr.append(next_pos)
 
-for time in B:
-    x = time
-    while x != 0:
-        if x > 0:
-            B_table.append(B_table[-1] + 1)
-            x -= 1
-        else : 
-            B_table.append(B_table[-1] - 1)
-            x += 1  
-# 만나는 시간을 기록할 변수를 둔다.
-meeting_time = -1
-# 한 인덱스로, 두 배열을 순회한다.
-for i in range(1, len(A_table)):
-    # A와 B가 만나는 지점이 있다면, 
-    if(A_table[i] == B_table[i]):
-        # 그 시간을 기록한다.
-        meeting_time = i
-        # 반복문을 종료한다.
+set_pos(pos_a, N)
+set_pos(pos_b, M)
+
+ans = -1
+
+for i in range(1, len(pos_a)):
+    if pos_a[i] == pos_b[i]:
+        ans = i
         break
-# 출력한다.
-print(meeting_time)
+
+print(ans)
