2637f19528f97d7a30772be28c7e149c537d7597
k-five-star <113240065+k-five-star@users.noreply.github.com>
2024-02-23 23:42:24 -0800

    Title: 벌금은 누구에게, Time: 179ms, Memory: 25MB, Status: Passed - CodeTree

--- 240224/벌금은 누구에게/who-will-pay.py
+++ 240224/벌금은 누구에게/who-will-pay.py
@@ -2,8 +2,8 @@
 num_of_student, n, limit = map(int, input().split())
 # 학생 수 크기의 배열을 만든다.
 student_list = [0] * (num_of_student + 1)
-# 플래그 초기화
-flag = 0
+# 정답 초기화
+ans = -1
 # 벌칙 횟수만큼의 반복문 실행
 for _ in range(n):
     # 학생 번호를 입력 받는다.
@@ -12,12 +12,9 @@ for _ in range(n):
     student_list[student_number] += 1
     # 해당 학생이 벌칙 임계치에 도달했는가?
     if student_list[student_number] >= limit:
-        # 학생 번호를 출력한다.
-        print(student_number)
-        # 플래그를 1로 설정하고
-        flag = 1
+        # 학생 번호를 정답으로 지정
+        ans = student_number
         # 반복문을 나간다. 
         break
-# 불린 학생이 없으면 -1 출력
-if flag == 0:
-    print(-1)
+# 정답 출력
+print(ans)
