377b8208387dd107cc509b690cf796e404fa048b
k-five-star <113240065+k-five-star@users.noreply.github.com>
2024-02-29 14:20:08 +0900

    Title: 좌우로 움직이는 로봇, Time: 137ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240229/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240229/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -23,6 +23,13 @@ for _ in range(m):
     for _ in range(int(sec)):
         B_cur_sec += 1
         B_sec_and_loc[B_cur_sec] = B_sec_and_loc[B_cur_sec - 1] + direction
+# 나머지 시간 채워야 한다. 
+if A_cur_sec > B_cur_sec:
+    for i in range(B_cur_sec + 1, A_cur_sec):
+        B_sec_and_loc[i] = B_sec_and_loc[B_cur_sec]
+else:
+    for i in range(A_cur_sec + 1, B_cur_sec):
+        A_sec_and_loc[i] = A_sec_and_loc[A_cur_sec]
 # 이 둘 사이의 거리 기록
 for i in range(0, max(A_cur_sec, B_cur_sec)):
     dist[i] = B_sec_and_loc[i] - A_sec_and_loc[i]
