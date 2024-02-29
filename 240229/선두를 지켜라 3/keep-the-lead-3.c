ebd2f7d7888508940f5e5df7df2cbc824b270360
k-five-star <113240065+k-five-star@users.noreply.github.com>
2024-02-29 17:33:12 +0900

    Title: 선두를 지켜라 3, Time: 9ms, Memory: 2MB, Status: Wrong Answer - CodeTree

--- 240229/선두를 지켜라 3/keep-the-lead-3.c
+++ 240229/선두를 지켜라 3/keep-the-lead-3.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-# define SIZE (1000*1000)
+#define SIZE (1000*1000)
 int a_pos[SIZE], b_pos[SIZE], gap[SIZE];
 
 int main() {
@@ -7,21 +7,25 @@ int main() {
 
     scanf("%d %d", &n, &m);
 
+    // 배열 초기화
+    a_pos[0] = 0;
+    b_pos[0] = 0;
+
     while(n--) {
         scanf("%d %d", &v, &t);
 
-        for(i = cur + 1; i <= cur + t; i++)
+        for(i = cur + 1; i <= cur + t && i < SIZE; i++) // SIZE를 초과하지 않도록 보호
             a_pos[i] = a_pos[i - 1] + v;
 
         cur = i - 1;
     }
 
     cur = 0;
-    
+
     while(m--) {
         scanf("%d %d", &v, &t);
 
-        for(i = cur + 1; i <= cur + t; i++) {
+        for(i = cur + 1; i <= cur + t && i < SIZE; i++) { // SIZE를 초과하지 않도록 보호
             b_pos[i] = b_pos[i - 1] + v;
             gap[i] = a_pos[i] - b_pos[i];
         }
@@ -30,9 +34,9 @@ int main() {
     }
 
     for(i = 1; i <= cur; i++) {
-        if(gap[i] != 0 && gap[i] * gap[i - 1] <= 0)  // 0이 아니라면, 곱했을 때 양수만 안나오면 다른거임
+        if (gap[i] != 0 && (i == 1 || gap[i] * gap[i - 1] <= 0))
             ans++;
-        if(gap[i] == 0 && gap[i - 1] != 0)
+        else if (gap[i] == 0 && gap[i - 1] != 0)
             ans++;
     }
 
