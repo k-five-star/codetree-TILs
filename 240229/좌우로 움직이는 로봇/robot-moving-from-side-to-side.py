6347cfd1125be75f4c526cc02e947d2cf85565a9
k-five-star <113240065+k-five-star@users.noreply.github.com>
2024-02-29 14:21:58 +0900

    Title: 좌우로 움직이는 로봇, Time: 114ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240229/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240229/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -15,6 +15,7 @@ for _ in range(n):
     for _ in range(int(sec)):
         A_cur_sec += 1
         A_sec_and_loc[A_cur_sec] = A_sec_and_loc[A_cur_sec - 1] + direction
+# print(A_sec_and_loc[0 : A_cur_sec + 1])
 # B로봇의 움직임 기록
 for _ in range(m):
     sec, cmd = input().split()
@@ -23,12 +24,13 @@ for _ in range(m):
     for _ in range(int(sec)):
         B_cur_sec += 1
         B_sec_and_loc[B_cur_sec] = B_sec_and_loc[B_cur_sec - 1] + direction
+# print(B_sec_and_loc[0 : B_cur_sec + 1])
 # 나머지 시간 채워야 한다. 
 if A_cur_sec > B_cur_sec:
-    for i in range(B_cur_sec + 1, A_cur_sec):
+    for i in range(B_cur_sec + 1, A_cur_sec + 1):
         B_sec_and_loc[i] = B_sec_and_loc[B_cur_sec]
 else:
-    for i in range(A_cur_sec + 1, B_cur_sec):
+    for i in range(A_cur_sec + 1, B_cur_sec + 1):
         A_sec_and_loc[i] = A_sec_and_loc[A_cur_sec]
 # 이 둘 사이의 거리 기록
 for i in range(0, max(A_cur_sec, B_cur_sec)):
