b94c407695ba497c467610c5a95edfdef652ca4e
k-five-star <113240065+k-five-star@users.noreply.github.com>
2024-02-29 17:16:45 +0900

    Title: 선두를 지켜라 3, Time: 274ms, Memory: 45MB, Status: Passed - CodeTree

--- 240229/선두를 지켜라 3/keep-the-lead-3.py
+++ 240229/선두를 지켜라 3/keep-the-lead-3.py
@@ -1,5 +1,3 @@
-## 알고리즘 ##
-# 입력을 받는다.
 N, M = map(int, input().split())
 
 def gen_pos_per_sec(cmd_arr):
@@ -12,18 +10,18 @@ def gen_pos_per_sec(cmd_arr):
 
 A_pos = gen_pos_per_sec([tuple(map(int, input().split())) for _ in range(N)])
 B_pos = gen_pos_per_sec([tuple(map(int, input().split())) for _ in range(M)])
-gap = [A_pos[i] - B_pos[i] for i in range(len(A_pos))]
 
 ans = 0
-past = 0
 
-for i in range(1, len(gap)):
-    if gap[i] != 0: # 양수거나, 음수거나. 바뀌었단건, 이전과 곱했을 때 0이거나 음수
-        if gap[i] * gap[i - 1] <= 0:
+for i in range(1, len(A_pos)):
+    gap = lambda x : A_pos[x] - B_pos[x]
+
+    if gap(i) != 0: # 양수거나, 음수거나. 바뀌었단건, 이전과 곱했을 때 0이거나 음수
+        if gap(i) * gap(i - 1) <= 0:
             ans += 1
     
     else: # 0이란 것. 이전에 0이 아니기만 하면 됨
-        if gap[i - 1] != 0:
+        if gap(i - 1) != 0:
             ans += 1
 
 print(ans)
